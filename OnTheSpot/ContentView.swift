2a153ec6989ec35f8478b233f201817b4bf65144
Kurt Gavin <kurtgavinlopez@gmail.com>
2026-01-08 17:36:23 +0800

    Initial Commit

---
+++ OnTheSpot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnTheSpot
+//
+//  Created by Kurt Gavin on 1/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnTheSpot/OnTheSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTheSpotApp.swift
+//  OnTheSpot
+//
+//  Created by Kurt Gavin on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct OnTheSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
